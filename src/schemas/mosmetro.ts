5910753d88c3d5dd8455713c0308720917c57789
corporateanon <corporateanon@gmail.com>
2022-01-24 10:01:32 +0000

    fix: remove parts of a tree-shaking approach

--- src/schemas/mosmetro.ts
+++
@@ -1,3 +0,0 @@
-import { Schema } from "../schema";
-import mosmetro from "../generated/mosmetro";
-export const MOSMETRO = Schema.load(mosmetro);
--- src/schemas/yandex_money.ts
+++
@@ -1,3 +0,0 @@
-import { Schema } from "../schema";
-import yandex_money from "../generated/yandex_money";
-export const YANDEX_MONEY = Schema.load(yandex_money);
